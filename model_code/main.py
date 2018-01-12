bef64360c29f7f15ea56b0fbb6c0a8b3fdc1ca26
sonsus <sison@bi.snu.ac.kr>
2018-01-12 18:45:48 +0900

    new exp setting

--- model_code/main.py
+++ model_code/main.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from glob import glob
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--dataset_name', dest='dataset_name', default='bolbbalgan4_organized', help='name of the dataset')
+parser.add_argument('--dataset_name', dest='dataset_name', default='bolbbalgan4', help='name of the dataset')
 parser.add_argument('--epoch', dest='epoch', type=int, default=200, help='# of epoch')
 parser.add_argument('--batch_size', dest='batch_size', type=int, default=1, help='# images in batch')
 parser.add_argument('--train_size', dest='train_size', type=int, default=1e8, help='# images used to train')
