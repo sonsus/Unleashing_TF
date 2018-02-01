b4166879f989a7b1dc90c0ac3941074af13667ef
sonsus <sison@bi.snu.ac.kr>
2018-02-01 19:01:38 +0900

    specgram code reviewed but key link is missing ( w/o core knowledge its hard ) --> use API

--- specgram_misc/specgram_gen.py
+++ specgram_misc/specgram_gen.py
@@ -7,35 +7,55 @@ import numpy
 import scipy.io.wavfile
 from scipy.fftpack import dct
 
-#open .wav file
-sample_rate, signal = scipy.io.wavfile.read('OSR_us_000_0010_8k.wav')  # File assumed to be in the same directory
-signal = signal[0:int(3.5 * sample_rate)]  # Keep the first 3.5 seconds
+#set specgram stats (units in seconds)
+frame_size=4.0
+frame_stride=0.5
 
-#pre_emphasis
-pre_emphasis = 0.97 #typically 0.95 or 0.97
-\ emphasized_signal = numpy.append(signal[0], signal[1:] - pre_emphasis * signal[:-1])
+
+#pre_emphasis: is a artifact from an old system but known to emphasize abit of high freq signals
+def preemp_sig(signal, preemp=0.97): #preemp = typically 0.95 or 0.97
+    emphasized_signal = numpy.append(signal[0], signal[1:] - preemp * signal[:-1])
+    return emphasized_signal
+
+#open .wav file
+sample_rate, signal = scipy.io.wavfile.read('f013.wav')  # File assumed to be in the same directory
+emphasized_signal=preemp_sig(signal)
+
+test10sec=emphasized_signal[:sample_rate*5]
 
 #framing
 frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate  # Convert from seconds to samples
-signal_length = len(emphasized_signal)
+signal_length = len(test10sec)
 frame_length = int(round(frame_length))
 frame_step = int(round(frame_step))
 num_frames = int(numpy.ceil(float(numpy.abs(signal_length - frame_length)) / frame_step))  # Make sure that we have at least 1 frame
+
+
+
+'''adding a pad for the last part of the signal: this part is not used since we do not use last part of the signal
+
 
 pad_signal_length = num_frames * frame_step + frame_length
 z = numpy.zeros((pad_signal_length - signal_length))
 pad_signal = numpy.append(emphasized_signal, z) # Pad Signal to make sure that all frames have equal number of samples without truncating any samples from the original signal
 
+'''
+
 indices = numpy.tile(numpy.arange(0, frame_length), (num_frames, 1)) + numpy.tile(numpy.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T
-frames = pad_signal[indices.astype(numpy.int32, copy=False)]
+frames = test10sec[indices.astype(numpy.int32, copy=False)]
+print("\temp sig shape")
+print(test10sec.shape)
+print("\tindices shape")
+print("\t", indices.shape)
 
 #windowing: hamming window is popular
+NFFT=1024
 frames *= numpy.hamming(frame_length) # frames *= 0.54 - 0.46 * numpy.cos((2 * numpy.pi * n) / (frame_length - 1))  # Explicit Implementation **
 mag_frames = numpy.absolute(numpy.fft.rfft(frames, NFFT))  # Magnitude of the FFT
 pow_frames = ((1.0 / NFFT) * ((mag_frames) ** 2))  # Power Spectrum
 
 #Filter Bank
-nfilt = 40
+nfilt = 1024
 low_freq_mel = 0
 high_freq_mel = (2595 * numpy.log10(1 + (sample_rate / 2) / 700))  # Convert Hz to Mel
 mel_points = numpy.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # Equally spaced in Mel scale
@@ -58,3 +78,17 @@ filter_banks = 20 * numpy.log10(filter_banks)  # dB
 
 # Mean normalization
 filter_banks -= (numpy.mean(filter_banks, axis=0) + 1e-8)
+print("\tfilterbank shape")
+print(filter_banks.shape)
+
+specgram=filter_bank.T @ pow_frames 
+
+
+def write_specgram_img(specgram, imgname):   #jpgname with .png
+#specgram here has the shape = (1024,1024,2)
+    fig, ax = plt.subplots(nrows=1,ncols=1)
+    #plt.axis("scaled")
+    cax = ax.matshow(np.transpose(specgram), interpolation='nearest', aspect='auto', cmap=plt.cm.afmhot, origin='lower')
+    fig.colorbar(cax)
+    plt.title('upper: generated_ensemble\n middle:original_ensemble\nlower: vocal_only')
+    plt.savefig(imgname, dpi="figure", bbox_inches="tight")
