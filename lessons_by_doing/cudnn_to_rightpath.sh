ff402598b3967a083b62c3518f6dcc1bd21a4fb7
SEONIL <seonilson92@gmail.com>
2018-04-24 13:33:36 +0900

    Update cudnn_to_rightpath.sh

--- lessons_by_doing/cudnn_to_rightpath.sh
+++ lessons_by_doing/cudnn_to_rightpath.sh
@@ -1,5 +1,10 @@
+nowadays, installing tensorflow-gpu will automatically resolve this problem
+
+
+
 #!/bin/bash
 
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+
